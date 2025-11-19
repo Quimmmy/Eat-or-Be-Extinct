9e9a87fe3615e3faa2724017c23a252dad564f6f
Kai-05-L <20240031050@my.xu.edu.ph>
2025-11-19 15:03:59 +0800

    Dinaurr movements

--- Eat or Be Extinct/Assets/Scripts/PlayerController.cs
+++ Eat or Be Extinct/Assets/Scripts/PlayerController.cs
@@ -3,21 +3,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float horizontalInput;
-    float moveSpeed = 10f;
-
+    public float moveSpeed = 10f;
     Rigidbody2D rb;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
     }
+
     private void FixedUpdate()
     {
-        rb.linearVelocityX = new Vector2 (horizontalInput* moveSpeed, rb.angularVelocity.y);
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
     }
 }
